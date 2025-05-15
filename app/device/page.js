caac1ac09f8e70875efdcc5d4c5e076aee94200e
Ridho Al Harits <ridhoalharits@gmail.com>
2025-05-16 01:38:55 +0700

    first stage

--- app/device/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>ini device</div>;
-};
-
-export default page;
--- app/home/page.js
+++ app/home/page.js
@@ -1,22 +1,11 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import { Switch } from "@/components/ui/switch";
 import NodeCard from "@/components/sensorComponent/nodeCard";
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const endpoint =
-    "https://mygarden-1c375-default-rtdb.asia-southeast1.firebasedatabase.app/.json";
+  const endpoint = "masukan endpoint disini"; // Replace with your actual endpoint
 
   const fetchData = async () => {
     try {
@@ -38,21 +27,14 @@ const Home = () => {
     };
   }, []);
 
-  // console.log(data);
+  console.log(data);
 
   return (
     <div className=" mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Sensor Data</h1>
+      {/* Tambahkan nama kalian disini */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data.map((item, index) => (
-          <NodeCard
-            key={index}
-            temperature={item.temperature}
-            humidity={item.humidity}
-            deviceName={item.deviceID}
-            ledStatus={item.led === 1 ? true : false}
-          />
-        ))}
+        {/* Import Component dan masukan data kedalam component */}
       </div>
     </div>
   );
